a41c0931016d92a50e3b8f246a94264d5dc1c4fa
amwexler <aaron.wexl@gmail.com>
2013-10-11 22:27:09 -0700

    Health bars are now proper colors depending on alliance
    Health bars are now proper colors depending on alliance

--- Armada/src/HealthBar.java
+++ Armada/src/HealthBar.java
@@ -22,19 +22,19 @@ public class HealthBar {
 	    g.setColor(Color.BLACK);
 		g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2), de.getY()-viewRect.getY() - de.getHeight()/2 -10, de.getWidth(), 3);
 		if(de.getAlliance()==1){
-			g.setColor(Color.BLUE);
+			g.setColor(Color.RED);
 		}
 		else if(de.getAlliance()==2){
-			g.setColor(Color.RED);
+			g.setColor(Color.BLUE);
 		}
 		g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2), de.getY()-viewRect.getY() - de.getHeight()/2-10, (int)((double)de.getWidth()*(double)((double)de.getHull()/(double)de.getMaxHull())), 3);
 		
 		
 		if(de.getAlliance()==1){
-			g.setColor(Color.CYAN);
+			g.setColor(new Color(250,100,100));
 		}
 		else if(de.getAlliance()==2){
-			g.setColor(new Color(250,100,100));
+			g.setColor(Color.CYAN);
 		}
 	    g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2) -1, de.getY()-viewRect.getY() - de.getHeight()/2 -1, de.getWidth()+3, 6);
 	    g.setColor(Color.BLACK);
