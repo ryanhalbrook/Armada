1915aece26c6482f7428c1e5438a2bfcb83cda0f
amwexler <aaron.wexl@gmail.com>
2013-10-11 22:30:07 -0700

    Health bars actually display proper colors now
    last time was a lie

--- Armada/src/HealthBar.java
+++ Armada/src/HealthBar.java
@@ -12,10 +12,10 @@ public class HealthBar {
 	
 	public void draw(Graphics g, Rectangle viewRect){
 		if(de.getAlliance()==1){
-			g.setColor(Color.BLUE);
+			g.setColor(Color.RED);
 		}
 		else if(de.getAlliance()==2){
-			g.setColor(Color.RED);
+			g.setColor(Color.BLUE);
 		}
 		
 	    g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2) -1, de.getY()-viewRect.getY() - de.getHeight()/2 -1 -10, de.getWidth()+3, 6);
@@ -40,10 +40,10 @@ public class HealthBar {
 	    g.setColor(Color.BLACK);
 		g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2), de.getY()-viewRect.getY() - de.getHeight()/2, de.getWidth(), 3);
 		if(de.getAlliance()==1){
-			g.setColor(Color.CYAN);
+			g.setColor(new Color(250,100,100));
 		}
 		else if(de.getAlliance()==2){
-			g.setColor(new Color(250,100,100));
+			g.setColor(Color.CYAN);
 		}
 		g.fillRect(de.getX()-viewRect.getX() - (de.getWidth()/2), de.getY()-viewRect.getY() - de.getHeight()/2, (int)((double)de.getWidth()*(double)((double)de.getEngine()/(double)de.getMaxEngine())), 3);
 		
