01aba787cbeae8185616db7ac689134618a4a844
Yun Suk Chang <yunsukchang@gmail.com>
2013-11-05 13:05:40 -0800

    There are now 8 different Explosion
    If u want a specific explosion use overloaded constructor (type=
    0,1,...,7)

--- Armada/src/Explosion.java
+++ Armada/src/Explosion.java
@@ -10,17 +10,22 @@ public class Explosion extends Element{
 	private static final int HEIGHT=64;
 	
 	private int imgIndex=0;
+	private int type;
 	public Explosion(DynamicElement d){
 		super(d.getX(),d.getY(),d.getWidth(),d.getHeight(),d.getAngle(),IMAGE_NAME);
-		
+		type=(int)(Math.random()*8);
+	}
+	public Explosion(DynamicElement d, int t){
+		super(d.getX(),d.getY(),d.getWidth(),d.getHeight(),d.getAngle(),IMAGE_NAME);
+		type=t;
 	}
 	public void draw(Graphics g, BoundingRectangle viewRegion){
-		BufferedImage temp= ah.getImage().getSubimage(WIDTH*imgIndex, 0, WIDTH, HEIGHT);
+		BufferedImage temp= ah.getImage().getSubimage(WIDTH*imgIndex, HEIGHT*type, WIDTH, HEIGHT);
 		AnimationHelper.draw(x, y, width, height, angle, temp, g, viewRegion);
 		imgIndex++;
 		
-
 	}
+	
 	public boolean isDone(){
 		return imgIndex>=NUM_FRAME;
 	}
