c2af81d0b5b7a26cc347222cfca1c675c120ff95
Ryan Halbrook <ryanhalbrook@umail.ucsb.edu>
2013-11-12 18:49:17 -0800

    Removed vast commented code

--- Armada/src/HUDmanager.java
+++ Armada/src/HUDmanager.java
@@ -32,122 +32,20 @@ public class HUDmanager extends ViewLayer {
 		stat.setName("Stat HUD");
 		turn.setName("Turn HUD");
 		items.setName("Items HUD");
-		//viewLayer = new ViewLayer(new BoundingRectangle(0, 0, grid.getAp().getWidth(), grid.getAp().getHeight()));
-		/*
-		addHUD(mode, Position.MODE_POSITION);
-		addHUD(stat, Position.TOP_RIGHT);
-		addHUD(map, Position.BOTTOM_RIGHT);
-		addHUD(items, Position.ITEM_POSITION);
-		addHUD(turn, Position.STATIC);
-		*/
+		
 		addHUD(stat);
 		addHUD(map);
 		addHUD(items);
 		addHUD(mode);
 		addHUD(turn);
 	}
-	/*
-	private void updateLocations() {
-	    for (HUD h : huds) {
-	        HUD.Position p = h.getPosition();
-	        BoundingRectangle r = h.getBoundingRectangle();
-	        if(gc != null) {
-		        if (p == HUD.Position.STATIC) continue;
-		        if (p == HUD.Position.TOP_LEFT) {
-			        r.setX(10);
-			        r.setY(TurnHUD.BAR_HEIGHT);
-			    }
-		        if (p == HUD.Position.TOP_RIGHT) {
-			        r.setX(gc.getViewWidth()-r.width);
-			        r.setY(TurnHUD.BAR_HEIGHT);
-			    }
-		        if (p == HUD.Position.BOTTOM_LEFT) {
-			        r.setX(10);
-			        r.setY(gc.getViewHeight() - r.height - 10);
-			    }
-		        if (p == HUD.Position.BOTTOM_RIGHT) {
-			        r.setX(gc.getViewWidth()-r.width - 10);
-			        r.setY(gc.getViewHeight() - r.height - 10);
-			    }
-		        if (p == HUD.Position.ITEM_POSITION) {
-			        r.setX(gc.getViewWidth()-r.width);
-			        r.setY(gc.getViewHeight()/2 - r.height/2);
-			    }
-			    if (p == HUD.Position.MODE_POSITION) {
-		            r.setX(0);
-		            r.setY(0);
-		        }
-		        if (p == HUD.Position.CENTERED) {
-		            System.out.println("CENTERED");
-		            r.setX(grid.getAp().getWidth()/2-r.width/2);
-		            r.setY(grid.getAp().getHeight()/2 - r.height/2);
-		        }
-		    }
-	    }
-	}
-	*/
-	
-	/*
-	private void updateLocations() {
-	    for (Position p: huds2.keySet()) {
-	        System.out.println(p);
-	        HUD h = huds2.get(p);
-	        BoundingRectangle r = h.getBoundingRectangle();
-	        if(gc != null) {
-		        if (p == Position.STATIC) continue;
-		        if (p == Position.TOP_LEFT) {
-			        r.setX(10);
-			        r.setY(TurnHUD.BAR_HEIGHT);
-			    }
-		        if (p == Position.TOP_RIGHT) {
-			        r.setX(gc.getViewWidth()-r.width);
-			        r.setY(TurnHUD.BAR_HEIGHT);
-			    }
-		        if (p == Position.BOTTOM_LEFT) {
-			        r.setX(10);
-			        r.setY(gc.getViewHeight() - r.height - 10);
-			    }
-		        if (p == Position.BOTTOM_RIGHT) {
-			        r.setX(gc.getViewWidth()-r.width - 10);
-			        r.setY(gc.getViewHeight() - r.height - 10);
-			    }
-		        if (p == Position.ITEM_POSITION) {
-			        r.setX(gc.getViewWidth()-r.width);
-			        r.setY(gc.getViewHeight()/2 - r.height/2);
-			    }
-			    if (p == Position.MODE_POSITION) {
-		            r.setX(0);
-		            r.setY(0);
-		        }
-		    }
-	    }
-	}
-	*/
-	/*
-	public HUDmanager(GameController gc) {
-	    super(new BoundingRectangle(
-	    huds2 = new Hashtable<Position, HUD>();
-	    this.gc = gc;
-	}
-	*/
 	
 	public void addHUD(HUD hud) {
-	huds.add(hud);
+	    huds.add(hud);
 	    addSublayer(hud);
-	    //viewLayer.addSublayer(hud);
 	}
-	
-	/**
-	    @return A view layer that draws all of the HUDs associated with this HUD system.
-	*/
-	/*
-	public ViewLayer getViewLayer() {
-	    return viewLayer;
-	}
-	*/
 	
 	public void refresh(long previousTime, long currentTime) {
-	    //updateLocations();
 	    super.refresh(previousTime, currentTime);
 	}
 	
@@ -172,11 +70,5 @@ public class HUDmanager extends ViewLayer {
 	    return (TurnHUD)turn;
 	}
 	
-	/*
-	public boolean click(int inX, int inY){
-		if(items.click(inX,inY))return true;
-		if(map.click(inX,inY))return true;
-		return false;
-	}
-	*/
+
 }
